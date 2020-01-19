83774449abf3e6af216f325c5c941e823d0ce8f9
meebs <alex@takayamasan.com>
2020-01-19 00:09:25 -0500

    play win noise upon impact with gate

--- Assets/Impact.cs
+++ Assets/Impact.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Impacts : MonoBehaviour
 {
     public AudioSource impact;
+    public AudioSource gate;
 
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision) 
     {
-        if (collision.collider.name != "Plane") {
+        string name = collision.collider.name;
+
+        if (name == "Gate") {
+            gate.Play();
+        } else if (name != "Plane") {
             impact.Play();
         }
     }

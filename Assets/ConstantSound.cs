3c467ad1484c34933efb8927eb0794f142318330
Arkin Mukherjee <amukherjee@benunets.com>
2020-01-19 02:21:48 -0500

    Add clock with sound

---
+++ Assets/ConstantSound.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockTick : MonoBehaviour
+{
+
+    private AudioSource tick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tick = GetComponent<AudioSource>();
+        tick.loop = true;
+        tick.Play();
+    }
+}
